cd62c3f1486dd13d2f803b241f729d17a259f7ac
Raam Dev <raam@raamdev.com>
2013-07-11 15:17:18 -0400

    Remove calls to wp_head() and wp_footer(); analytics shouldn't track this page

--- email-popup.php
+++ email-popup.php
@@ -27,12 +27,20 @@ add_filter('the_title', 'email_title');
 	<meta name="robots" content="noindex, nofollow" />
 	<title><?php bloginfo('name'); ?> <?php if ( is_single() ) { ?> &raquo; <?php _e('Blog Archive', 'wp-email'); ?> <?php } ?> <?php wp_title(); ?></title>
 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
+	<link rel='stylesheet' id='wp-email-css'  href='<?php bloginfo('stylesheet_directory'); ?>/email-css.css?ver=2.60' type='text/css' media='all' />
+	<script type='text/javascript' src='<?php bloginfo('wpurl'); ?>/wp-includes/js/jquery/jquery.js?ver=1.8.3'></script>
 	<?php wp_head(); ?>
 </head>
 <body class="wp-email">
 	<div id="wp-email-popup">
 		<?php email_form(true); ?>
-		<?php wp_footer(); ?>
+		<?php //wp_footer(); ?>
 	</div>
+	<script type='text/javascript'>
+	/* <![CDATA[ */
+	var emailL10n = {"ajax_url":"http:\/\/raamdev.dev\/wordpress\/wp-admin\/admin-ajax.php","max_allowed":"5","text_error":"The Following Error Occurs:","text_name_invalid":"- Your Name is empty\/invalid","text_email_invalid":"- Your Email is empty\/invalid","text_remarks_invalid":"- Your Remarks is invalid","text_friend_names_empty":"- Friend Name(s) is empty","text_friend_name_invalid":"- Friend Name is empty\/invalid: ","text_max_friend_names_allowed":"- Maximum 5 Friend Names allowed","text_friend_emails_empty":"- Friend Email(s) is empty","text_friend_email_invalid":"- Friend Email is invalid: ","text_max_friend_emails_allowed":"- Maximum 5 Friend Emails allowed","text_friends_tally":"- Friend Name(s) count does not tally with Friend Email(s) count","text_image_verify_empty":"- Image Verification is empty"};
+	/* ]]> */
+	</script>
+	<script type='text/javascript' src='<?php bloginfo('wpurl'); ?>/wp-content/plugins/wp-email/email-js.js?ver=2.60'></script>
 </body>
 </html>
